83bc9025988ea044ddb24a4e232539fa349089ef
nicol4sb <nicolasb@gmail.com>
2018-01-17 15:04:52 +0100

    Adding the NDA edition flow

--- java-source/src/main/java/com/example/api/ExampleApi.java
+++ java-source/src/main/java/com/example/api/ExampleApi.java
@@ -7,6 +7,7 @@ import static javax.ws.rs.core.Response.Status.CREATED;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
+import java.util.function.Predicate;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
@@ -16,17 +17,20 @@ import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import org.apache.commons.collections.CollectionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.example.flow.ExampleFlow;
 import com.example.flow.NDACreationFlow;
+import com.example.flow.NDAEditionFlow;
 import com.example.state.IOUState;
 import com.example.state.NDAContractState;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 
 import net.corda.core.contracts.StateAndRef;
+import net.corda.core.contracts.UniqueIdentifier;
 import net.corda.core.identity.CordaX500Name;
 import net.corda.core.identity.Party;
 import net.corda.core.messaging.CordaRPCOps;
@@ -37,144 +41,190 @@ import net.corda.core.transactions.SignedTransaction;
 // This API is accessible from /api/example. All paths specified below are relative to it.
 @Path("example")
 public class ExampleApi {
-    private final CordaRPCOps rpcOps;
-    private final CordaX500Name myLegalName;
+	private final CordaRPCOps rpcOps;
+	private final CordaX500Name myLegalName;
 
-    private final List<String> serviceNames = ImmutableList.of("Controller", "Network Map Service");
+	private final List<String> serviceNames = ImmutableList.of("Controller", "Network Map Service");
 
-    static private final Logger logger = LoggerFactory.getLogger(ExampleApi.class);
+	static private final Logger logger = LoggerFactory.getLogger(ExampleApi.class);
 
-    public ExampleApi(CordaRPCOps rpcOps) {
-        this.rpcOps = rpcOps;
-        this.myLegalName = rpcOps.nodeInfo().getLegalIdentities().get(0).getName();
-    }
+	public ExampleApi(CordaRPCOps rpcOps) {
+		this.rpcOps = rpcOps;
+		this.myLegalName = rpcOps.nodeInfo().getLegalIdentities().get(0).getName();
+	}
 
-    /**
-     * Returns the node's name.
-     */
-    @GET
-    @Path("me")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Map<String, CordaX500Name> whoami() {
-        return ImmutableMap.of("me", myLegalName);
-    }
+	/**
+	 * Returns the node's name.
+	 */
+	@GET
+	@Path("me")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Map<String, CordaX500Name> whoami() {
+		return ImmutableMap.of("me", myLegalName);
+	}
 
-    /**
-     * Returns all parties registered with the [NetworkMapService]. These names can be used to look up identities
-     * using the [IdentityService].
-     */
-    @GET
-    @Path("peers")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Map<String, List<CordaX500Name>> getPeers() {
-        List<NodeInfo> nodeInfoSnapshot = rpcOps.networkMapSnapshot();
-        return ImmutableMap.of("peers", nodeInfoSnapshot
-                .stream()
-                .map(node -> node.getLegalIdentities().get(0).getName())
-                .filter(name -> !name.equals(myLegalName) && !serviceNames.contains(name.getOrganisation()))
-                .collect(toList()));
-    }
+	/**
+	 * Returns all parties registered with the [NetworkMapService]. These names can
+	 * be used to look up identities using the [IdentityService].
+	 */
+	@GET
+	@Path("peers")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Map<String, List<CordaX500Name>> getPeers() {
+		List<NodeInfo> nodeInfoSnapshot = rpcOps.networkMapSnapshot();
+		return ImmutableMap.of("peers",
+				nodeInfoSnapshot.stream().map(node -> node.getLegalIdentities().get(0).getName())
+						.filter(name -> !name.equals(myLegalName) && !serviceNames.contains(name.getOrganisation()))
+						.collect(toList()));
+	}
 
-    /**
-     * Displays all IOU states that exist in the node's vault.
-     */
-    @GET
-    @Path("ious")
-    @Produces(MediaType.APPLICATION_JSON)
-    public List<StateAndRef<IOUState>> getIOUs() {
-        return rpcOps.vaultQuery(IOUState.class).getStates();
-    }
+	/**
+	 * Displays all NDA states that exist in the node's vault.
+	 */
+	@GET
+	@Path("ious")
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<StateAndRef<IOUState>> getIOUs() {
+		return rpcOps.vaultQuery(IOUState.class).getStates();
+	}
 
-    /**
-     * Initiates a flow to agree an IOU between two parties.
-     *
-     * Once the flow finishes it will have written the IOU to ledger. Both the lender and the borrower will be able to
-     * see it when calling /api/example/ious on their respective nodes.
-     *
-     * This end-point takes a Party name parameter as part of the path. If the serving node can't find the other party
-     * in its network map cache, it will return an HTTP bad request.
-     *
-     * The flow is invoked asynchronously. It returns a future when the flow's call() method returns.
-     */
-    @PUT
-    @Path("create-iou")
-    public Response createIOU(@QueryParam("iouValue") int iouValue, @QueryParam("partyName") CordaX500Name partyName) throws InterruptedException, ExecutionException {
-        if (iouValue <= 0) {
-            return Response.status(BAD_REQUEST).entity("Query parameter 'iouValue' must be non-negative.\n").build();
-        }
-        if (partyName == null) {
-            return Response.status(BAD_REQUEST).entity("Query parameter 'partyName' missing or has wrong format.\n").build();
-        }
+	/**
+	 * Initiates a flow to agree an IOU between two parties.
+	 *
+	 * Once the flow finishes it will have written the IOU to ledger. Both the
+	 * lender and the borrower will be able to see it when calling /api/example/ious
+	 * on their respective nodes.
+	 *
+	 * This end-point takes a Party name parameter as part of the path. If the
+	 * serving node can't find the other party in its network map cache, it will
+	 * return an HTTP bad request.
+	 *
+	 * The flow is invoked asynchronously. It returns a future when the flow's
+	 * call() method returns.
+	 */
+	@PUT
+	@Path("create-iou")
+	public Response createIOU(@QueryParam("iouValue") int iouValue, @QueryParam("partyName") CordaX500Name partyName)
+			throws InterruptedException, ExecutionException {
+		if (iouValue <= 0) {
+			return Response.status(BAD_REQUEST).entity("Query parameter 'iouValue' must be non-negative.\n").build();
+		}
+		if (partyName == null) {
+			return Response.status(BAD_REQUEST).entity("Query parameter 'partyName' missing or has wrong format.\n")
+					.build();
+		}
 
-        final Party otherParty = rpcOps.wellKnownPartyFromX500Name(partyName);
-        if (otherParty == null) {
-            return Response.status(BAD_REQUEST).entity("Party named " + partyName + "cannot be found.\n").build();
-        }
+		final Party otherParty = rpcOps.wellKnownPartyFromX500Name(partyName);
+		if (otherParty == null) {
+			return Response.status(BAD_REQUEST).entity("Party named " + partyName + "cannot be found.\n").build();
+		}
 
-        try {
-            FlowProgressHandle<SignedTransaction> flowHandle = rpcOps
-                    .startTrackedFlowDynamic(ExampleFlow.Initiator.class, iouValue, otherParty);
-            flowHandle.getProgress().subscribe(evt -> System.out.printf(">> %s\n", evt));
+		try {
+			FlowProgressHandle<SignedTransaction> flowHandle = rpcOps
+					.startTrackedFlowDynamic(ExampleFlow.Initiator.class, iouValue, otherParty);
+			flowHandle.getProgress().subscribe(evt -> System.out.printf(">> %s\n", evt));
 
-            // The line below blocks and waits for the flow to return.
-            final SignedTransaction result = flowHandle
-                    .getReturnValue()
-                    .get();
+			// The line below blocks and waits for the flow to return.
+			final SignedTransaction result = flowHandle.getReturnValue().get();
 
-            final String msg = String.format("Transaction id %s committed to ledger.\n", result.getId());
-            return Response.status(CREATED).entity(msg).build();
+			final String msg = String.format("Transaction id %s committed to ledger.\n", result.getId());
+			return Response.status(CREATED).entity(msg).build();
 
-        } catch (Throwable ex) {
-            final String msg = ex.getMessage();
-            logger.error(ex.getMessage(), ex);
-            return Response.status(BAD_REQUEST).entity(msg).build();
-        }
-    }
-    
-    
-    @PUT
-    @Path("create-ndarequest")
-    public Response createNDARequest(@QueryParam("ndaRequestText") String ndaRequestText, @QueryParam("partyName") CordaX500Name partyName) throws InterruptedException, ExecutionException {
-        if ( !(ndaRequestText.length() > 20) ) {
-            return Response.status(BAD_REQUEST).entity("Query parameter 'ndaRequestText' - Put some text in this NDA ! More than 20 chars.\n").build();
-        }
-        if (partyName == null) {
-            return Response.status(BAD_REQUEST).entity("Query parameter 'partyName' missing or has wrong format.\n").build();
-        }
+		} catch (Throwable ex) {
+			final String msg = ex.getMessage();
+			logger.error(ex.getMessage(), ex);
+			return Response.status(BAD_REQUEST).entity(msg).build();
+		}
+	}
 
-        // Translate the string into a valid Party from the central node/notary service
-        final Party otherParty = rpcOps.wellKnownPartyFromX500Name(partyName);
-        if (otherParty == null) {
-            return Response.status(BAD_REQUEST).entity("Party named " + partyName + "cannot be found.\n").build();
-        }
+	@PUT
+	@Path("create-ndarequest")
+	public Response createNDARequest(@QueryParam("ndaRequestText") String ndaRequestText,
+			@QueryParam("partyName") CordaX500Name partyName) throws InterruptedException, ExecutionException {
+		if (!(ndaRequestText.length() > 20)) {
+			return Response.status(BAD_REQUEST)
+					.entity("Query parameter 'ndaRequestText' - Put some text in this NDA ! More than 20 chars.\n")
+					.build();
+		}
+		if (partyName == null) {
+			return Response.status(BAD_REQUEST).entity("Query parameter 'partyName' missing or has wrong format.\n")
+					.build();
+		}
 
-        try {
-        		System.out.println("NDARequestText : "+ndaRequestText+" -- otherParty :: "+partyName);
-            FlowProgressHandle<SignedTransaction> flowHandle = rpcOps
-                    .startTrackedFlowDynamic(NDACreationFlow.Initiator.class, ndaRequestText, otherParty);
-            flowHandle.getProgress().subscribe(evt -> System.out.printf(">> %s\n", evt));
+		// Translate the string into a valid Party from the central node/notary service
+		final Party otherParty = rpcOps.wellKnownPartyFromX500Name(partyName);
+		if (otherParty == null) {
+			return Response.status(BAD_REQUEST).entity("Party named " + partyName + "cannot be found.\n").build();
+		}
 
-            // The line below blocks and waits for the flow to return.
-            final SignedTransaction result = flowHandle
-                    .getReturnValue()
-                    .get();
+		try {
+			System.out.println("NDARequestText : " + ndaRequestText + " -- otherParty :: " + partyName);
+			FlowProgressHandle<SignedTransaction> flowHandle = rpcOps
+					.startTrackedFlowDynamic(NDACreationFlow.Initiator.class, ndaRequestText, otherParty);
+			flowHandle.getProgress().subscribe(evt -> System.out.printf(">> %s\n", evt));
 
-            final String msg = String.format("NDA request with id %s committed to ledger.\n", result.getId());
-            return Response.status(CREATED).entity(msg).build();
+			// The line below blocks and waits for the flow to return.
+			final SignedTransaction result = flowHandle.getReturnValue().get();
 
-        } catch (Throwable ex) {
-            final String msg = ex.getMessage();
-            logger.error(ex.getMessage(), ex);
-            return Response.status(BAD_REQUEST).entity(msg).build();
-        }
-    }
+			final String msg = String.format("NDA request with id %s committed to ledger.\n", result.getId());
+			return Response.status(CREATED).entity(msg).build();
 
-    @GET
-    @Path("getNdaRequests")
-    @Produces(MediaType.APPLICATION_JSON)
-    public List<StateAndRef<NDAContractState>> getNdaRequests() {
-        return rpcOps.vaultQuery(NDAContractState.class).getStates();
-    }
+		} catch (Throwable ex) {
+			final String msg = ex.getMessage();
+			logger.error(ex.getMessage(), ex);
+			return Response.status(BAD_REQUEST).entity(msg).build();
+		}
+	}
 
+	@GET
+	@Path("getNdaRequests")
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<StateAndRef<NDAContractState>> getNdaRequests() {
+		return rpcOps.vaultQuery(NDAContractState.class).getStates();
+	}
+
+	@PUT
+	@Path("review-nda")
+	public Response reviewNDA(@QueryParam("ndaRequestText") String ndaRequestText,
+			@QueryParam("ndaPreviousStateId") String ndaPreviousStateId)
+			throws InterruptedException, ExecutionException {
+		if (!(ndaRequestText.length() > 20)) {
+			return Response.status(BAD_REQUEST)
+					.entity("Query parameter 'ndaRequestText' - Put some text in this NDA ! More than 20 chars.\n")
+					.build();
+		}
+		if (ndaPreviousStateId == null) {
+			return Response.status(BAD_REQUEST).entity("Query parameter 'ndaPreviousStateId' missing.\n").build();
+		}
+
+		// Translate the previous state id into a the state object
+		List<StateAndRef<NDAContractState>> previousStates = rpcOps.vaultQuery(NDAContractState.class).getStates();
+		NDAContractState previousNDAContractState = null;
+
+		for (StateAndRef<NDAContractState> stateAndRef : previousStates) {
+			if (stateAndRef.getState().getData().getLinearId().toString().equals(ndaPreviousStateId)) {
+				previousNDAContractState = stateAndRef.getState().getData();
+			}
+		}
+
+		try {
+			System.out
+					.println("NDARequestText : " + ndaRequestText + " -- ndaPreviousStateId :: " + ndaPreviousStateId);
+			FlowProgressHandle<SignedTransaction> flowHandle = rpcOps
+					.startTrackedFlowDynamic(NDAEditionFlow.Initiator.class, ndaRequestText, previousNDAContractState);
+			flowHandle.getProgress().subscribe(evt -> System.out.printf(">> %s\n", evt));
+
+			// The line below blocks and waits for the flow to return.
+			final SignedTransaction result = flowHandle.getReturnValue().get();
+
+			final String msg = String.format("NDA request with id %s committed to ledger.\n", result.getId());
+			return Response.status(CREATED).entity(msg).build();
+
+		} catch (Throwable ex) {
+			final String msg = ex.getMessage();
+			logger.error(ex.getMessage(), ex);
+			return Response.status(BAD_REQUEST).entity(msg).build();
+		}
+	}
 
 }
---
+++ java-source/src/main/java/com/example/flow/NDAEditionFlow.java
@@ -0,0 +1,161 @@
+package com.example.flow;
+
+import static net.corda.core.contracts.ContractsDSL.requireThat;
+
+import java.util.stream.Collectors;
+
+import com.example.contract.NDAContract;
+import com.example.state.NDAContractState;
+import com.google.common.collect.Sets;
+
+import co.paralleluniverse.fibers.Suspendable;
+import net.corda.core.contracts.Command;
+import net.corda.core.contracts.ContractState;
+import net.corda.core.contracts.StateAndContract;
+import net.corda.core.flows.CollectSignaturesFlow;
+import net.corda.core.flows.FinalityFlow;
+import net.corda.core.flows.FlowException;
+import net.corda.core.flows.FlowLogic;
+import net.corda.core.flows.FlowSession;
+import net.corda.core.flows.InitiatedBy;
+import net.corda.core.flows.InitiatingFlow;
+import net.corda.core.flows.SignTransactionFlow;
+import net.corda.core.flows.StartableByRPC;
+import net.corda.core.identity.AbstractParty;
+import net.corda.core.identity.Party;
+import net.corda.core.transactions.SignedTransaction;
+import net.corda.core.transactions.TransactionBuilder;
+import net.corda.core.utilities.ProgressTracker;
+import net.corda.core.utilities.ProgressTracker.Step;
+
+/**
+ * This flow allows two parties (the [Initiator] and the [Acceptor]) to come to an agreement about the NDARequest encapsulated
+ * within an [NDARquestState].
+ * <p>
+ * An NDARequest is always accepted by the recipient for now
+ * <p>
+ * These flows have deliberately been implemented by using only the call() method for ease of understanding. In
+ * practice we would recommend splitting up the various stages of the flow into sub-routines.
+ * <p>
+ * All methods called within the [FlowLogic] sub-class need to be annotated with the @Suspendable annotation.
+ */
+public class NDAEditionFlow {
+    @InitiatingFlow
+    @StartableByRPC
+    public static class Initiator extends FlowLogic<SignedTransaction> {
+        
+        private final String ndaRequestText;
+        private final NDAContractState previousNDAContractState;
+
+        private final Step GENERATING_TRANSACTION = new Step("Generating transaction based on new NDA edition Request.");
+        private final Step VERIFYING_TRANSACTION = new Step("Verifying contract constraints.");
+        private final Step SIGNING_TRANSACTION = new Step("Signing transaction with our private key.");
+        private final Step GATHERING_SIGS = new Step("Gathering the counterparty's signature.") {
+            @Override public ProgressTracker childProgressTracker() {
+                return CollectSignaturesFlow.Companion.tracker();
+            }
+        };
+        private final Step FINALISING_TRANSACTION = new Step("Obtaining notary signature and recording transaction.") {
+            @Override public ProgressTracker childProgressTracker() {
+                return FinalityFlow.Companion.tracker();
+            }
+        };
+        
+        // The progress tracker checkpoints each stage of the flow and outputs the specified messages when each
+        // checkpoint is reached in the code. See the 'progressTracker.currentStep' expressions within the call()
+        // function.
+        private final ProgressTracker progressTracker = new ProgressTracker(
+                GENERATING_TRANSACTION,
+                VERIFYING_TRANSACTION,
+                SIGNING_TRANSACTION,
+                GATHERING_SIGS,
+                FINALISING_TRANSACTION
+        );
+
+        public Initiator(String ndaRequestText, NDAContractState previousNDAContractState) {
+            this.ndaRequestText= ndaRequestText;
+            this.previousNDAContractState = previousNDAContractState;
+        }
+
+        @Override
+        public ProgressTracker getProgressTracker() {
+            return progressTracker;
+        }
+
+        /**
+         * The flow logic is encapsulated within the call() method.
+         */
+        @Suspendable
+        @Override
+        public SignedTransaction call() throws FlowException {
+            // Obtain a reference to the notary we want to use.
+            final Party notary = getServiceHub().getNetworkMapCache().getNotaryIdentities().get(0);
+            
+            // Stage 1.
+            progressTracker.setCurrentStep(GENERATING_TRANSACTION);
+            // Generate an unsigned transaction.
+            NDAContractState ndaContractState = new NDAContractState(ndaRequestText, getServiceHub().getMyInfo().getLegalIdentities().get(0), previousNDAContractState.getNdaRequestRecipient());
+            
+            final Command<NDAContract.Commands.Create> txCommand = new Command<>(new NDAContract.Commands.Create(),
+                    ndaContractState.getParticipants().stream().map(AbstractParty::getOwningKey).collect(Collectors.toList()));
+            
+            final TransactionBuilder txBuilder = new TransactionBuilder(notary).withItems(new StateAndContract(ndaContractState, NDAContract.NDA_CONTRACT_ID), txCommand);
+
+            // Stage 2.
+            progressTracker.setCurrentStep(VERIFYING_TRANSACTION);
+            // Verify that the transaction is valid.
+            txBuilder.verify(getServiceHub());
+
+            // Stage 3.
+            progressTracker.setCurrentStep(SIGNING_TRANSACTION);
+            // Sign the transaction.
+            final SignedTransaction partSignedTx = getServiceHub().signInitialTransaction(txBuilder);
+
+            FlowSession otherPartySession = initiateFlow(previousNDAContractState.getNdaRequestRecipient());
+
+            // Stage 4.
+            progressTracker.setCurrentStep(GATHERING_SIGS);
+            // Send the state to the counterparty, and receive it back with their signature.
+            final SignedTransaction fullySignedTx = subFlow(
+                    new CollectSignaturesFlow(partSignedTx, Sets.newHashSet(otherPartySession), CollectSignaturesFlow.Companion.tracker()));
+
+            // Stage 5.
+            progressTracker.setCurrentStep(FINALISING_TRANSACTION);
+            // Notarise and record the transaction in both parties' vaults.
+            return subFlow(new FinalityFlow(fullySignedTx));
+        }
+    }
+
+    @InitiatedBy(Initiator.class)
+    public static class Acceptor extends FlowLogic<SignedTransaction> {
+
+        private final FlowSession otherPartyFlow;
+
+        public Acceptor(FlowSession otherPartyFlow) {
+            this.otherPartyFlow = otherPartyFlow;
+        }
+
+        @Suspendable
+        @Override
+        public SignedTransaction call() throws FlowException {
+            class SignTxFlow extends SignTransactionFlow {
+                private SignTxFlow(FlowSession otherPartyFlow, ProgressTracker progressTracker) {
+                    super(otherPartyFlow, progressTracker);
+                }
+
+                @Override
+                protected void checkTransaction(SignedTransaction stx) {
+                    requireThat(require -> {
+                        ContractState output = stx.getTx().getOutputs().get(0).getData();
+                        require.using("This must be an NDA request state transaction.", output instanceof NDAContractState);
+                        NDAContractState ndaRequest = (NDAContractState) output;
+                        require.using("I won't accept NDA requests with a text smaller than 20 chars.", ndaRequest.getNdaRequestText().length() > 20);
+                        return null;
+                    });
+                }
+            }
+
+            return subFlow(new SignTxFlow(otherPartyFlow, SignTransactionFlow.Companion.tracker()));
+        }
+    }
+}
